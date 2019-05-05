0834f5298d779f8f5452a206e33c654ad87f0e93
ERIC <baha2046>
2019-05-06 00:38:14 +0900

    updated

--- japsearch/src/main/java/org/nagoya/system/cache/MovieCache.java
+++ japsearch/src/main/java/org/nagoya/system/cache/MovieCache.java
@@ -1,12 +1,13 @@
 package org.nagoya.system.cache;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import io.vavr.control.Option;
 import org.nagoya.model.MovieV2;
 
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
-public class MovieCache extends CaffeineCache<Path, MovieV2> {
+public class MovieCache extends CaffeineCache<Path, Option<MovieV2>> {
 
     MovieCache() {
         this.cache = Caffeine.newBuilder()
